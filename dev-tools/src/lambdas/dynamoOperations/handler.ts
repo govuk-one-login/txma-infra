dfe35a9b0ce62cec80c1d5156171a159993a95f7
mabon-gds <mabon.gwyn@digital.cabinet-office.gov.uk>
2023-03-09 11:05:54 +0000

    addressed comments

--- dev-tools/src/lambdas/dynamoOperations/handler.ts
+++ dev-tools/src/lambdas/dynamoOperations/handler.ts
@@ -25,15 +25,15 @@ export const handler = async (
   switch (dynamoOperationParams.operation) {
     case 'GET':
       result = await dynamoDbGet(dynamoOperationParams.params)
-      logger.info('Sent GetItemCommand to Dynamo')
+      logger.info('GetItemCommand successfully sent to Dynamo')
       break
     case 'PUT':
       result = await dynamoDbPut(dynamoOperationParams.params)
-      logger.info('Sent PutItemCommand to Dynamo')
+      logger.info('PutItemCommand successfully sent to Dynamo')
       break
     case 'DELETE':
       result = await dynamoDbDelete(dynamoOperationParams.params)
-      logger.info('Sent DeleteItemCommand to Dynamo')
+      logger.info('DeleteItemCommand successfully sent to Dynamo')
       break
     default:
       throw Error('Dynamo operation not recognised')
