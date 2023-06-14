9d7bcc0b7ca4c454ef89eac42dafeaea3738ae43
mabon-gds <mabon.gwyn@digital.cabinet-office.gov.uk>
2023-06-14 17:12:53 +0100

    refactor get for consistency

--- dev-tools/src/lambdas/dynamoOperations/dynamoDbGet.ts
+++ dev-tools/src/lambdas/dynamoOperations/dynamoDbGet.ts
@@ -3,14 +3,16 @@ import { OperationParams } from '../../types/dynamoDbOperation'
 import { documentClient } from './dynamoDbClient'
 
 export const dynamoDbGet = async (operationParams: OperationParams) => {
-  const res = (await documentClient.send(
-    new GetCommand({
-      TableName: operationParams.tableName,
-      Key: operationParams.key,
-      ...(operationParams.desiredAttributeName && {
-        ProjectionExpression: operationParams.desiredAttributeName
-      })
+  const getCommand = {
+    TableName: operationParams.tableName,
+    Key: operationParams.key,
+    ...(operationParams.desiredAttributeName && {
+      ProjectionExpression: operationParams.desiredAttributeName
     })
+  }
+
+  const res = (await documentClient.send(
+    new GetCommand(getCommand)
   )) as GetCommandOutput
 
   return res.Item
