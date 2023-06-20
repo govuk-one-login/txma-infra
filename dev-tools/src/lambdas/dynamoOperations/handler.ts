87e529b40030870e0842bee5c2e58feddee6f81a
Andy Langridge <andy.langridge@digital.cabinet-office.gov.uk>
2023-06-20 14:50:18 +0100

    Log params on individual basis

--- dev-tools/src/lambdas/dynamoOperations/handler.ts
+++ dev-tools/src/lambdas/dynamoOperations/handler.ts
@@ -19,15 +19,21 @@ export const handler = async (
   switch (dynamoDbOperation.operation) {
     case 'GET':
       result = await dynamoDbGet(dynamoDbOperation.params)
-      logger.info('GetItemCommand successfully sent to Dynamo')
+      logger.info('GetItemCommand successfully sent to Dynamo', {
+        params: dynamoDbOperation.params
+      })
       break
     case 'PUT':
       result = await dynamoDbPut(dynamoDbOperation.params)
-      logger.info('PutItemCommand successfully sent to Dynamo')
+      logger.info('PutItemCommand successfully sent to Dynamo', {
+        params: dynamoDbOperation.params
+      })
       break
     case 'DELETE':
       result = await dynamoDbDelete(dynamoDbOperation.params)
-      logger.info('DeleteItemCommand successfully sent to Dynamo')
+      logger.info('DeleteItemCommand successfully sent to Dynamo', {
+        params: dynamoDbOperation.params
+      })
       break
     default:
       throw Error('Dynamo operation not recognised')
