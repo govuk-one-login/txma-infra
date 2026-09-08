70f25d504b407ab10be356518b7794ea4787808d
James Holland <james.holland@digital.cabinet-office.gov.uk>
2026-09-08 15:57:48 +0100

    fixed issue with build

--- infrastructure/dev-tools/src/utils/logger.ts
+++ infrastructure/dev-tools/src/utils/logger.ts
@@ -1,1 +1,12 @@
 export { logger, initialiseLogger } from '@govuk-one-login/dpt-logging'
+import { logger } from '@govuk-one-login/dpt-logging'
+
+export const removeLoggerKeys = (keys: string[]) => {
+  logger.removeKeys(keys)
+}
+
+export const appendKeyAttributeDataToLogger = (
+  key: Record<string, unknown>
+) => {
+  logger.appendKeys(key)
+}
