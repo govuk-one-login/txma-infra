8e7730acef026579c58f6650b0e01a097b2ac670
Pradeep Swamireddy <pradeep.swamireddy1@digital.cabinet-office.gov.uk>
2026-09-08 12:31:07 +0100

    DPT-3268 emptyS3Bucket succeeds if bucket doesnt exist

--- infrastructure/dev-tools/src/lambdas/emptyS3Buckets/emptyS3Bucket.ts
+++ infrastructure/dev-tools/src/lambdas/emptyS3Buckets/emptyS3Bucket.ts
@@ -1,8 +1,20 @@
-import { DeleteObjectCommand, S3Client } from '@aws-sdk/client-s3'
+import {
+  DeleteObjectCommand,
+  HeadBucketCommand,
+  S3Client
+} from '@aws-sdk/client-s3'
 import { logger } from '../../utils/logger.js'
 import { listS3ObjectVersions } from './listS3ObjectVersions.js'
+import { getEnv } from '../../utils/getEnv.js'
 
 export const emptyS3Bucket = async (bucketName: string): Promise<void> => {
+  const s3Client = new S3Client({ region: getEnv('AWS_REGION') })
+
+  if (!(await bucketExists(s3Client, bucketName))) {
+    logger.info('Bucket does not exist, skipping', { bucketName })
+    return
+  }
+
   const objects = await listS3ObjectVersions({ Bucket: bucketName })
   const objectCount = objects.versions.length + objects.deleteMarkers.length
 
@@ -10,20 +22,57 @@ export const emptyS3Bucket = async (bucketName: string): Promise<void> => {
 
   if (objects.versions.length === 0 && objects.deleteMarkers.length === 0) {
     logger.info('No objects to delete', { bucketName })
-  } else {
-    await Promise.all(
-      objects.deleteMarkers.map((object) =>
-        deleteObject(bucketName, object.Key, object.VersionId)
-      )
-    )
-    await Promise.all(
-      objects.versions.map((object) =>
-        deleteObject(bucketName, object.Key, object.VersionId)
-      )
-    )
-
-    logger.info('Successfully emptied S3 bucket', { bucketName })
+    return
   }
+
+  await Promise.all(
+    objects.deleteMarkers.map((object) =>
+      deleteObject(bucketName, object.Key, object.VersionId)
+    )
+  )
+  await Promise.all(
+    objects.versions.map((object) =>
+      deleteObject(bucketName, object.Key, object.VersionId)
+    )
+  )
+
+  logger.info('Successfully emptied S3 bucket', { bucketName })
+}
+
+const bucketExists = async (
+  s3Client: S3Client,
+  bucketName: string
+): Promise<boolean> => {
+  try {
+    await s3Client.send(new HeadBucketCommand({ Bucket: bucketName }))
+    return true
+  } catch (error: unknown) {
+    if (isBucketNotFoundError(error)) {
+      logger.info('Bucket not found', { bucketName })
+      return false
+    }
+
+    logger.error('Unable to access bucket', {
+      bucketName,
+      error: error instanceof Error ? error.name : 'Unknown error'
+    })
+    throw error
+  }
+}
+
+const isBucketNotFoundError = (error: unknown): boolean => {
+  if (!(error instanceof Error)) {
+    return false
+  }
+
+  const statusCode = (error as { $metadata?: { httpStatusCode?: number } })
+    .$metadata?.httpStatusCode
+
+  return (
+    error.name === 'NotFound' ||
+    error.name === 'NoSuchBucket' ||
+    statusCode === 404
+  )
 }
 
 const deleteObject = async (
